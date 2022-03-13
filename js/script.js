7dcc8de3d3ea66f8ca4fb98a184e449cd4916d05
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-14 00:07:05 +0300

    initial js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-function myFunction
+$(document).ready(function(){
+   $("#design-image").click();{
+       $("#design-image").hide()
+       $("#design").show();
+   };
+    alert("hello")
+   $("#design").click() ;{
+       $("#design").hide()
+       $("#seigin-image").show();
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+});
